0bc390585a2c9e974d03d15bd6fa8cd3036336d3
DAM1V03 <dam1v03idat@gmail.com>
2024-02-19 14:25:03 -0500

    primer commit

---
+++ app/src/main/java/pe/edu/idat/apprecyclerviewsimple/MainActivity.kt
@@ -0,0 +1,31 @@
+package pe.edu.idat.apprecyclerviewsimple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import pe.edu.idat.apprecyclerviewsimple.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvlista.layoutManager = LinearLayoutManager(
+            applicationContext)
+        binding.rvlista.adapter = AdapterLista(listado())
+    }
+
+    private fun listado(): List<String> {
+        val lista = ArrayList<String>()
+        var i = 0
+        while(i < 10){
+            lista.add("Valor $i")
+            i++
+        }
+        return lista
+    }
+
+}
